43abc342466fbe68499d82cd40c3306344cc3d21
SamukaAlves000 <60469274+SamukaAlves000@users.noreply.github.com>
2020-02-03 09:45:57 -0300

    Criação do Projeto
    Iniciando criação do Projeto Spring Boot

---
+++ politico_na_mao/src/test/java/com/samuel/politico_na_mao/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuel.politico_na_mao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
